314a3a78ae738574036fa344082d94f2576d9b1a
plastina <plastina.bp@gmail.com>
2023-09-24 10:41:04 -0300

    criação das particulas pion, kaon , d0 e lambda. Inserção do decaimento destas partículas. Padronização dos pontilhados antes da curvatura

--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,6 @@
 let particles = [];
 // const numParticles = 100;
-const particleTypes = ["electron", "positron", "proton", "antiproton", "muon", "photon"];
+const particleTypes = ["electron", "positron", "proton", "antiproton", "muon", "photon", "pion", "kaon"];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -53,6 +53,7 @@ function fireParticle(particleType) {
     yPosition = 400;
     xVelocity = 10;
     yVelocity = 1;
+    Xacceleration = 0.5;
 
   } else if (particleType === "muon") {
     xPosition = 70;
@@ -65,10 +66,40 @@ function fireParticle(particleType) {
   } else if (particleType === "photon") {
     xPosition = 70;
     yPosition = 400;
-    xVelocity = 1;
+    xVelocity = 20;
     yVelocity = 0;
     // Xacceleration = 1;
     // Yacceleration = 0.5;
+
+  } else if (particleType === "pion") {
+    xPosition = 70;
+    yPosition = 400;
+    xVelocity = 10;
+    yVelocity = 0;
+    Xacceleration = 0.5;
+    Yacceleration = 0;
+
+  } else if (particleType === "kaon") {
+    xPosition = 70;
+    yPosition = 400;
+    xVelocity = 10;
+    yVelocity = 0;
+    Xacceleration = 0.5;
+    Yacceleration = 0;
+  } else if (particleType === "lambda") {
+    xPosition = 70;
+    yPosition = 400;
+    xVelocity = 10;
+    yVelocity = 0;
+    Xacceleration = 0.5;
+    Yacceleration = 0;
+  } else if (particleType === "d0") {
+    xPosition = 70;
+    yPosition = 400;
+    xVelocity = 10;
+    yVelocity = 0;
+    Xacceleration = 0.5;
+    Yacceleration = 0;
   }
 
   let particle = {
@@ -94,7 +125,6 @@ function drawParticles() {
       particle.velocity.y = -5;
       particle.velocity.x = 15;
 
-
       if (particle.position.x > 1000) {
         particles.splice(i, 1);
         i--;
@@ -108,12 +138,10 @@ function drawParticles() {
       particle.velocity.y = -1;
       particle.velocity.x = 15;
 
-
       if (particle.position.x > 1000) {
         particles.splice(i, 1);
         i--;
       }
-
     }
 
     if (particle.type === "proton" && particle.position.x > 500) {
@@ -121,7 +149,6 @@ function drawParticles() {
       particle.velocity.y = 1;
       particle.acceleration.x = 5;
       particle.acceleration.y = -0.5;
-
 
       if (particle.position.x > 1100) {
         particles.splice(i, 1);
@@ -131,10 +158,9 @@ function drawParticles() {
 
     if (particle.type === "antiproton" && particle.position.x > 500) {
       particle.velocity.x = 3;
-      particle.velocity.y = -1;
+      particle.velocity.y = -0.5;
       particle.acceleration.x = 5;
       particle.acceleration.y = -0.5;
-
 
       if (particle.position.x > 1100) {
         particles.splice(i, 1);
@@ -145,11 +171,76 @@ function drawParticles() {
     if (particle.type === "photon" && particle.position.x > 500) {
       particle.velocity.x = 20;
 
-
       if (particle.position.x > 1000) {
         particles.splice(i, 1);
         i--;
       }
+    }
+
+    if (particle.type === "pion" && particle.position.x > 500) {
+      particle.velocity.x = 3;
+      particle.velocity.y = 1;
+      particle.acceleration.x = 5;
+      particle.acceleration.y = -0.7;
+
+      if (particle.position.x > 1100) {
+        particles.splice(i, 1);
+        i--;
+      }
+    }
+
+    if (particle.type === "kaon" && particle.position.x > 500) {
+      particle.velocity.x = 3;
+      particle.velocity.y = 1;
+      particle.acceleration.x = 5;
+      particle.acceleration.y = -0.5;
+
+      if (particle.position.x > 1100) {
+        particles.splice(i, 1);
+        i--;
+      }
+    }
+
+    if (particle.type === "lambda" && particle.position.x > 500) {
+      let particle1 = {
+        type: "proton",
+        position: createVector(particle.position.x, particle.position.y),
+        velocity: createVector(5, 0),
+        acceleration: createVector(1, 0)
+      };
+      particles.push(particle1);
+      
+      let particle2 = {
+        type: "pion",
+        position: createVector(particle.position.x, particle.position.y),
+        velocity: createVector(5, 0),
+        acceleration: createVector(1, 0)
+      };
+      particles.push(particle2);
+      
+      particles.splice(i, 1);
+      i--;
+    }
+
+    if (particle.type === "d0" && particle.position.x > 500) {
+      let particle3 = {
+        type: "kaon",
+        position: createVector(particle.position.x, particle.position.y),
+        velocity: createVector(5, 0),
+        acceleration: createVector(1, 0)
+      };
+      particles.push(particle3);
+      
+      let particle2 = {
+        type: "pion",
+        position: createVector(particle.position.x, particle.position.y),
+        velocity: createVector(5, 0),
+        acceleration: createVector(1, 0)
+      };
+      particles.push(particle2);
+      
+      particles.splice(i, 1);
+      i--;
     }
 
 
@@ -171,17 +262,25 @@ function drawParticles() {
 function getParticleColor(particleType) {
   switch (particleType) {
     case "electron":
-      return color(65,105,225);
+      return color(65, 105, 225);
     case "positron":
-      return color(0,0,139);
+      return color(0, 0, 139);
     case "proton":
-      return color(244,164,96);
+      return color(244, 164, 96);
     case "antiproton":
-      return color(255,222,173);
+      return color(255, 222, 173);
     case "muon":
-      return color(0,100,0);
+      return color(0, 100, 0);
     case "photon":
-      return color(50,205,50);
+      return color(50, 205, 50);
+    case "pion":
+      return color(250, 210, 35);
+    case "kaon":
+      return color(200, 203, 42);
+    case "lambda":
+      return color(244, 164, 96);
+    case "d0":
+      return color(244, 164, 96);
   }
 }
 
@@ -199,5 +298,13 @@ function getParticleClass(particleType) {
       return ".muon";
     case "photon":
       return ".photon";
+    case "pion":
+      return ".pion";
+    case "kaon":
+      return ".kaon";
+    case "lambda":
+      return ".lambda";
+    case "d0":
+      return ".d0";
   }
 }
